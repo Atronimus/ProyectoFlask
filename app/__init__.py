43f1e2cf2be72c76e398c791196c80782a93761d
Sistemas Suferre <lsistemas@suferre.com>
2025-12-09 14:27:31 -0600

    adicion de una secret key valida

--- app/__init__.py
+++ app/__init__.py
@@ -9,8 +9,8 @@ def create_app():
     app = Flask(__name__)
 
     # Configuración
-    app.config['SECRET_KEY'] = 'clave-super-secreta'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1/proyectoflask'
+    app.config['SECRET_KEY'] = '317fa9ea388016ac94c3b441b9c75236778a35964c889f31b51c688d06d24bf8'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dev:S4wmizry6irgtCn+@127.0.0.1:3306/proyectoflask'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Inicializar extensiones
